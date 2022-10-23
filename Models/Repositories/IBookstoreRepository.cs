7781a9de9325c9aa796bd6f7b786812ad0f40c13
elsaftawy <mohamedelsftawy7944@gmail.com>
2022-10-23 19:42:14 +0200

    Create Book Repo

---
+++ Models/Repositories/IBookstoreRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Models.Repositories
+{
+    interface IBookstoreRepository<TEntity>
+    {
+        IList<TEntity> List();
+        TEntity Find(int id);
+
+        void Add(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(int id);
+    }
+}
